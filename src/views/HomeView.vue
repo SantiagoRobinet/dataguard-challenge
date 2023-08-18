dbe4f6cb1e36ee52bef8c9aa61fb1815021ec4db
Santiago Robinet <hope@MacBook-Pro-de-Santiago.local>
2023-08-18 15:21:52 +0200

    chore: refactor styles, and cleanup initial config

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h2>Tab Name</h2>
+    <div>
+      All the different plugins
+    </div>
   </main>
 </template>
